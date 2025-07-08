7e995e9082a72b3711a5b5c343b71a902861d118
Jiri Hala <hala.jiri@gmail.com>
2025-07-08 16:29:39 +0200

    API: set-up mapping (DTO)

---
+++ BeeApp.Api/Mapping/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BeeApp.Shared.DTO;
+using BeeApp.Shared.Models;
+
+namespace BeeApp.Api.Mapping
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Apiary, ApiaryDto>().ReverseMap();
+            CreateMap<Hive, HiveDto>().ReverseMap();
+            // others future mapping
+        }
+    }
+}
