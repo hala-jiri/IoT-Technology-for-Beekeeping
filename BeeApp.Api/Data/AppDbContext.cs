ecaf826b0d9d1a2d0f4f563cb7c4edef2ef81950
Jiri Hala <hala.jiri@gmail.com>
2025-07-08 16:48:13 +0200

    API: set up DbContext and connectionstring for dev.

--- BeeApp.Api/Data/AppDbContext.cs
+++ BeeApp.Api/Data/AppDbContext.cs
@@ -9,9 +9,10 @@ namespace BeeApp.Api.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Apiary> Apiaries { get; set; }
+        public DbSet<ApiaryMeasurement> ApiaryMeasurements { get; set; }
         public DbSet<Hive> Hives { get; set; }
         public DbSet<HiveMeasurement> HiveMeasurements { get; set; }
-        public DbSet<ApiaryMeasurement> ApiaryMeasurements { get; set; }
+        public DbSet<HiveNote> HiveNotes { get; set; }
         public DbSet<InspectionReport> InspectionReports { get; set; }
         public DbSet<WarehouseItem> WarehouseItems { get; set; }
         public DbSet<WarehouseItemUsage> WarehouseItemUsages { get; set; }
--- BeeApp.Api/Program.cs
+++ BeeApp.Api/Program.cs
@@ -1,4 +1,6 @@
+using BeeApp.Api.Data;
 using BeeApp.Api.Mapping;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(MappingProfile));
-
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
