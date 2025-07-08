1de317accfc11e2a06abf3285823eaad40aee513
Jiri Hala <hala.jiri@gmail.com>
2025-07-08 16:27:00 +0200

    Add AppDbContext

---
+++ BeeApp.Api/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BeeApp.Shared.DTO;
+using BeeApp.Shared.Models;
+
+namespace BeeApp.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Apiary> Apiaries { get; set; }
+        public DbSet<Hive> Hives { get; set; }
+        public DbSet<HiveMeasurement> HiveMeasurements { get; set; }
+        public DbSet<ApiaryMeasurement> ApiaryMeasurements { get; set; }
+        public DbSet<InspectionReport> InspectionReports { get; set; }
+        public DbSet<WarehouseItem> WarehouseItems { get; set; }
+        public DbSet<WarehouseItemUsage> WarehouseItemUsages { get; set; }
+    }
+}
