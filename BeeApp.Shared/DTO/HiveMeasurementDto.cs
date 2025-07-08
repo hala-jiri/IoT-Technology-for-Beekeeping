78865abe2fe5c5c90ee3daa9fd30c17f67691a3b
Jiri Hala <hala.jiri@gmail.com>
2025-07-08 17:03:12 +0200

    Shared: add DTO classes

---
+++ BeeApp.Shared/DTO/HiveMeasurementDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeApp.Shared.DTO
+{
+    public class HiveMeasurementDto
+    {
+        public int HiveId { get; set; }
+        public double Weight { get; set; }
+        public double Temperature { get; set; }
+        public int Humidity { get; set; }
+    }
+}
---
+++ BeeApp.Shared/DTO/MeasurementDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeApp.Shared.DTO
+{
+    public class MeasurementDto
+    {
+        public int ApiaryId { get; set; }
+        public DateTime? MeasurementDate { get; set; }
+
+        public double Temperature { get; set; }
+        public int Humidity { get; set; }
+        public int LightIntensity { get; set; }
+
+        public List<HiveMeasurementDto> Hives { get; set; }
+    }
+}
