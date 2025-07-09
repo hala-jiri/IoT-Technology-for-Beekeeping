b1297c60fa684541636d31d28042452abb391046
Jiri Hala <hala.jiri@gmail.com>
2025-07-09 09:01:57 +0200

    Apiary UI and VM

---
+++ BeeApp.Shared/ViewModels/ApiaryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeApp.Shared.ViewModels
+{
+    public class ApiaryViewModel
+    {
+        public int ApiaryId { get; set; }
+        public string Name { get; set; }
+        public int HiveCount { get; set; }
+        public DateTime? LastMeasurement { get; set; }
+    }
+}
---
+++ BeeApp.Web/Controllers/ApiaryController.cs
@@ -0,0 +1,38 @@
+﻿using BeeApp.Shared.Data;
+using BeeApp.Shared.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace BeeApp.Web.Controllers
+{
+    public class ApiaryController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ApiaryController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var apiaries = await _context.Apiaries
+                .Include(a => a.Hives)
+                .Include(a => a.Measurements)
+                .ToListAsync();
+
+            var vm = apiaries.Select(a => new ApiaryViewModel
+            {
+                ApiaryId = a.ApiaryId,
+                Name = a.Name ?? string.Empty,
+                HiveCount = a.Hives.Count,
+                LastMeasurement = a.Measurements
+                    .OrderByDescending(m => m.MeasurementDate)
+                    .FirstOrDefault()?.MeasurementDate
+            }).ToList();
+
+            return View(vm);
+        }
+    }
+}
