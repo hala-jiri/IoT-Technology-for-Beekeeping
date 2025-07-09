500045837d85988174bd12afacbe18d4d695aa4e
Jiri Hala <hala.jiri@gmail.com>
2025-07-09 09:14:46 +0200

    View/controller/VM - List of hives in apiary

---
+++ BeeApp.Shared/ViewModels/HiveViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeApp.Shared.ViewModels
+{
+    public class HiveViewModel
+    {
+        public int HiveId { get; set; }
+        public string Name { get; set; }
+        public int ApiaryId { get; set; }
+        public string ApiaryName { get; set; }
+        public DateTime? LastMeasurement { get; set; }
+        public double? LastWeight { get; set; }
+        public double? LastTemperature { get; set; }
+    }
+}
---
+++ BeeApp.Web/Controllers/HiveController.cs
@@ -0,0 +1,39 @@
+﻿using BeeApp.Shared.Data;
+using BeeApp.Shared.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeeApp.Web.Controllers
+{
+    public class HiveController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public HiveController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int apiaryNumber)
+        {
+            var hives = await _context.Hives
+                .Include(h => h.Apiary)
+                .Include(h => h.Measurements)
+                .Where(h => h.ApiaryId == apiaryNumber)
+                .ToListAsync();
+
+            var vm = hives.Select(h => new HiveViewModel
+            {
+                HiveId = h.HiveId,
+                Name = h.Name,
+                ApiaryId = h.ApiaryId,
+                ApiaryName = h.Apiary.Name,
+                LastMeasurement = h.Measurements.OrderByDescending(m => m.MeasurementDate).FirstOrDefault()?.MeasurementDate,
+                LastWeight = h.Measurements.OrderByDescending(m => m.MeasurementDate).FirstOrDefault()?.Weight,
+                LastTemperature = h.Measurements.OrderByDescending(m => m.MeasurementDate).FirstOrDefault()?.Temperature
+            }).ToList();
+
+            return View(vm);
+        }
+    }
+}
