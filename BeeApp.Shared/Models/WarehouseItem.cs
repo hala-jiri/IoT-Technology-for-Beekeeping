ce66012fff7fd9dca344afdebea988c11f5710b9
Jiri Hala <hala.jiri@gmail.com>
2025-07-15 13:14:43 +0200

    WarehouseItem model and troubles with migration DB

--- BeeApp.Shared/Models/WarehouseItem.cs
+++ BeeApp.Shared/Models/WarehouseItem.cs
@@ -9,16 +9,27 @@ namespace BeeApp.Shared.Models
 {
     public class WarehouseItem
     {
-        [Key]
-        public int ItemId { get; set; }
+        public int WarehouseItemId { get; set; }
 
         [Required]
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;  // např. "Stropní krmítko"
 
         public string? Description { get; set; }
 
-        public int QuantityInStock { get; set; }
+        public int Quantity { get; set; }
 
-        public List<WarehouseItemUsage> Usages { get; set; } = new();
+        public ItemStatus Status { get; set; } = ItemStatus.Available;
+
+        public string? LocationNote { get; set; } // např. "Apiary 1", "sklad 2"
+
+        public DateTime Created { get; set; } = DateTime.Now;
+    }
+
+    public enum ItemStatus
+    {
+        Available = 0,
+        InUse = 1,
+        Damaged = 2,
+        Discarded = 3
     }
 }
