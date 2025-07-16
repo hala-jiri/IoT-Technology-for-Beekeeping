20fa983d141122c717a869e3cedc2d6dc0a4d5a7
Jiri Hala <hala.jiri@gmail.com>
2025-07-16 10:29:09 +0200

    Export do JSONu

--- BeeApp.Web/Controllers/ExportController.cs
+++ BeeApp.Web/Controllers/ExportController.cs
@@ -28,7 +28,7 @@ namespace BeeApp.Web.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Export(ExportDataRequestViewModel request)
+        public async Task<IActionResult> ExportCsv(ExportDataRequestViewModel request)
         {
             var from = request.From ?? DateTime.MinValue;
             var to = request.To ?? DateTime.MaxValue;
@@ -88,6 +88,67 @@ namespace BeeApp.Web.Controllers
             var zipBytes = memoryStream.ToArray();
             var fileName = $"export-{DateTime.Now:yyyyMMddHHmmss}.zip";
             return File(zipBytes, "application/zip", fileName);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ExportJson(ExportDataRequestViewModel request)
+        {
+            var from = request.From ?? DateTime.MinValue;
+            var to = request.To ?? DateTime.MaxValue;
+
+            var selectedHives = await _context.Hives
+                .Include(h => h.Measurements)
+                .Include(h => h.InspectionReports)
+                .Where(h => request.SelectedHiveIds.Contains(h.HiveId))
+                .ToListAsync();
+
+            var exportObject = new
+            {
+                From = from,
+                To = to,
+                ExportedAt = DateTime.Now,
+                Hives = selectedHives.Select(h => new
+                {
+                    HiveId = h.HiveId,
+                    HiveName = h.Name,
+                    Measurements = request.IncludeMeasurements
+                        ? h.Measurements
+                            .Where(m => m.MeasurementDate >= from && m.MeasurementDate <= to)
+                            .OrderBy(m => m.MeasurementDate)
+                            .Select(m => new
+                            {
+                                m.MeasurementDate,
+                                m.Weight,
+                                m.Temperature
+                            })
+                        : null,
+                    Inspections = request.IncludeInspections
+                        ? h.InspectionReports
+                            .Where(i => i.InspectionDate >= from && i.InspectionDate <= to)
+                            .OrderBy(i => i.InspectionDate)
+                            .Select(i => new
+                            {
+                                i.InspectionDate,
+                                i.QueenSeen,
+                                i.BroodPresent,
+                                i.EggsPresent,
+                                i.PollenPresent,
+                                i.HoneyPresent,
+                                i.Notes
+                            })
+                        : null
+                })
+            };
+
+            var json = System.Text.Json.JsonSerializer.Serialize(exportObject, new System.Text.Json.JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+
+            var fileName = $"export-{DateTime.Now:yyyyMMddHHmmss}.json";
+            var jsonBytes = Encoding.UTF8.GetBytes(json);
+
+            return File(jsonBytes, "application/json", fileName);
         }
     }
 }
