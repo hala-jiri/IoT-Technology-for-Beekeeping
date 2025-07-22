6194b11e077d65f10dbee463415f3c1aadfbaedd
Jiri Hala <hala.jiri@gmail.com>
2025-07-22 07:50:38 +0200

    Api-Backup

---
+++ BeeApp.Api/Controllers/ApiBackupController.cs
@@ -0,0 +1,39 @@
+﻿using BeeApp.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeeApp.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BackupController : ControllerBase
+    {
+        private readonly BackupService _backupService;
+        private readonly IConfiguration _config;
+
+        public BackupController(BackupService backupService, IConfiguration config)
+        {
+            _backupService = backupService;
+            _config = config;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBackup()
+        {
+            //TODO: for test purposes
+            var requestKey = Request.Headers["X-Api-Key"].ToString();
+            var configuredKey = _config["Backup:ApiKey"];
+
+            if (string.IsNullOrWhiteSpace(configuredKey) || requestKey != configuredKey)
+            {
+                return Unauthorized("Invalid API key");
+            }
+
+            var backup = await _backupService.CreateBackupAsync();
+
+            if (backup.Success)
+                return Ok(new { success = true, file = backup.FileName });
+            else
+                return StatusCode(500, new { success = false, message = backup.Message });
+        }
+    }
+}
--- BeeApp.Api/Program.cs
+++ BeeApp.Api/Program.cs
@@ -1,5 +1,6 @@
 using BeeApp.Api.Mapping;
 using BeeApp.Shared.Data;
+using BeeApp.Web.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +13,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<BackupService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
